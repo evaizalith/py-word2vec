Fix backprop

src/corpus.py
@@ -36,8 +36,8 @@ class Language():
         except ValueError:
             raise Exception(f"Item '{x}' is not in the vocabulary")
 
-        hot = np.zeros(shape=(self.corpus_size, 1))
-        hot[index] = 1
+        hot = np.zeros(shape=(1, self.corpus_size))
+        hot[0][index] = 1
 
         return hot
 

src/nn.py
@@ -1,44 +1,113 @@
 import numpy as np
 
+# Decorator to check for NaN values in numpy arrays sent to and returned from functions
+# Used to help debug 
+def nanlog(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        has_nan = None
+
+        types = [type(arg) for arg in args]
+        ndarrays = list(filter(lambda x: type(x) == "ndarray", types))
+
+        for ndarray in ndarrays:
+            has_nan = np.isnan(ndarray).any()
+            if has_nan:
+                break
+
+        if has_nan:
+            print(f"Array in func '{func}' contains NaN: {args}")
+
+        has_nan = None
+
+        if type(result) == "ndarray":
+            has_nan = np.isnan(result).any()
+
+        if has_nan:
+            print(f"Array from func '{func}' contains NaN: {result}")
+
+        return result
+    return wrapper
+
 class linear():
     def __init__(self, n: int, m: int):
-        r = np.random.uniform(-1, 1, size=(n * m))
-        self.A = r.reshape(n, m)
+        self.A = np.random.uniform(-1, 1, (n, m))
+        self.b = np.random.rand(1, int(m))
+        self.x = None
 
     def __call__(self, x):
-        x = np.dot(x.T, self.A)
-        return x.T
+        self.x = x 
+        x = np.dot(x, self.A) + self.b
+        return x
+
+    # Calculate and apply gradient for this layer then pass on new derivative to next layer 
+    def backward(self, d, lr, l):
+        grad_A = self.x.T @ d
+        grad_b = d.sum(axis=0)
+        
+        #print(f"d{l} : {d}")
+        dx = d @ self.A.T
+
+        self.A -= grad_A * lr
+        self.b -= grad_b * lr
+
+        return dx
+
+class ReLU():
+    def forward(self, z):
+        self.mask = (z > 0).astype(float)
+        return np.maximum(z, 0)
+
+    def backward(self, d):
+        return d * self.mask
+
+    def __call__(self, z):
+        return self.forward(z)
 
 # Neural Network base class
 # Defines the general structure of a neural network and all essential functionality
 class net():
     def __init__(self, n: int, h: int, m: int):
         self.fc1 = linear(n, h)
+        self.relu1 = ReLU()
         self.fc2 = linear(h, h)
+        self.relu2 = ReLU()
         self.fc3 = linear(h, m)
 
+    @nanlog
     def forward(self, x):
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
+        x = self.relu1(self.fc1(x))
+        x = self.relu2(self.fc2(x))
         x = self.softmax(self.fc3(x))
         return x
 
-    # Uses stochastic gradient descent 
-    def gradient(self, x, y):
-        x
+    # Uses stochastic gradient descent and applies loss
+    @nanlog
+    def gradient(self, x, y_pred, y_true, loss, lr):
+        # Derivative of softmax + cross entropy
+        d = y_pred - y_true
+        print(f"y_pred: {y_pred}")
+        print(f"y_true: {y_true}")
+        print(f"Initial d: {d}")
 
-    # Applies results of loss 
-    def backward(self, loss, lr):
-        x
+        # Each call to Linear.backward() calculates the gradient of the layer, applies it, and then returns it so that the next layer can use it as the basis for its own gradient calculations
+        d = self.fc3.backward(d, lr, 3)
+        d = self.relu2.backward(d)
+        d = self.fc2.backward(d, lr, 2)
+        d = self.relu1.backward(d)
+        d = self.fc1.backward(d, lr, 1)
 
     @staticmethod
+    @nanlog
     def softmax(z):
+        #print(z)
         e = np.exp(z - np.max(z))
         sigma = e / e.sum()
 
-        return sigma
+        return sigma 
 
     @staticmethod
+    @nanlog
     def relu(x):
         return np.maximum(x, 0)
 
@@ -48,8 +117,9 @@ class net():
         return np.square(s).sum()
 
     @staticmethod
-    def cross_entropy(p, q):
-       return 
+    @nanlog
+    def cross_entropy(x, y):
+       return -np.sum((1 - x) * np.log(y + 1e-8)) 
 
 def test():
     print("Test ReLU...")